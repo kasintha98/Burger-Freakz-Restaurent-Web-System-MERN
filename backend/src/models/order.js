167d2fbb6b1b16a54e9437a55d79a4ec1dcbd9d8
Kasintha Kalhara <kasinthakalhara@gmail.com>
2021-07-10 16:55:38 +0530

    Order Id - short id done

--- backend/src/models/order.js
+++ backend/src/models/order.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const ID = require("nodejs-unique-numeric-id-generator");
 
 const orderSchema = new mongoose.Schema(
   {
+    _id: {
+      type: String,
+      default: ID.generate(new Date().toJSON()),
+    },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
