4e52076c8345c03c2a9d5469499534a5fc93be1f
Kasintha Kalhara <kasinthakalhara@gmail.com>
2021-07-14 22:47:44 +0530

    Reports started

---
+++ admin-app/src/components/Reports/OrderReport.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OrderReport() {
+  return <div>ord</div>;
+}
---
+++ admin-app/src/components/Reports/UserReport.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UserReport() {
+  return <div>user</div>;
+}
